9f86fe4b57653e22cd4faba5f574f0a009f2cbb2
AnasAhmedFND <anasahmed4402@gmail.com>
2024-11-14 21:05:57 +0600

    footer

--- my-project/src/components/Footer.jsx
+++ my-project/src/components/Footer.jsx
@@ -1,25 +1,65 @@
 import React from 'react'
-
+import Logo from '../assets/Logo.png'
+import { IoMdMail } from "react-icons/io"
+import { IoIosCall } from "react-icons/io"
+import { IoLocationSharp } from "react-icons/io5"
+import { FaFacebook } from "react-icons/fa"
+import { FaTwitter } from "react-icons/fa"
+import { FaLinkedin } from "react-icons/fa"
 const Footer = () => {
   return (
     <>
-      <div className=" flex justify-around border-2 bg-primary text-2xl mt-16">
-        <div className="">
-            <p>hello1</p>
-            <p>hello2</p>
-            <p>hello3</p>
+    <div className="">
+      <div className="footer flex justify-between mt-16 border-b-2 pb-8">
+        <div className="one">
+          <img src={Logo} alt="" />
+          <div className="contact">
+             <div className="flex  items-center gap-3 mt-8">
+             <p><IoMdMail /></p>
+             <p>hello@skillbridge.com</p>
+             </div>
+
+             <div className="flex items-center gap-3">
+                <p><IoIosCall /></p>
+                <p>+9191813 23 2309</p>
+             </div>
+
+             <div className="flex gap-3 items-center">
+              <p><IoLocationSharp /></p>
+              <p>Somewhere in the World</p>
+             </div>
+          </div>
         </div>
-        <div className="">
-            <p>hello4</p>
-            <p>hello2</p>
-            <p>hello3</p>
+
+        <div className="home text-start">
+          <h2 className='font-bold'>Home</h2>
+          <p className='mt-5'>Benefits</p>
+          <p>Our courses</p>
+          <p>Our Testimonials</p>
+          <p>Our RAQ</p>
         </div>
-        <div className="">
-            <p>hello5</p>
-            <p>hello2</p>
-            <p>hello3</p>
+
+        <div className="about text-start">
+          <h2 className='font-bold'>About Us</h2>
+          <p className='mt-5'>Company</p>
+          <p>Achievements</p>
+          <p>Our Goals</p>
         </div>
+
+        <div className="social">
+          <h2 className='font-bold'>Social Profiles</h2>
+          <div className="flex gap-2 mt-5">
+            <p className='border w-8 h-8 justify-items-center align-middle pt-2'><FaFacebook /></p>
+            <p className='border w-8 h-8 justify-items-center align-middle pt-2'><FaTwitter /></p>
+            <p className='border w-8 h-8 justify-items-center align-middle pt-2'><FaLinkedin /></p>
+          </div>
+        </div>
+
       </div>
+
+      <p className='pt-8'>© 2023 Skillbridge. All rights reserved.</p>
+
+    </div>
     </>
   )
 }
--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import { FaArrowRight } from "react-icons/fa"
 const Header = () => {
   return (
     <>
-      <div className="">
-        <h2 className='border p-5 text-center font-bold bg-primary text-white font-vietnam'>Free Courses 🌟 Sale Ends Soon, Get It Now </h2>
+      <div className="flex border p-5 bg-primary text-center items-center gap-5 justify-center">
+        <h2 className='text-center font-bold  text-white font-vietnam'>Free Courses 🌟 Sale Ends Soon, Get It Now </h2>
+        <p className='text-white'><FaArrowRight /></p>
       </div>
     </>
   )
